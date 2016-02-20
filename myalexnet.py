ed455ca57acd2a119e7a14ef18ffe1b4471049ff
Michael Guerzhoy <guerzh@gmail.com>
2016-02-20 01:17:04 -0500

    first commit

---
+++ myalexnet.py
@@ -0,0 +1,62 @@
+from numpy import *
+import tensorflow as tf
+
+# (self.feed('data')
+#         .conv(11, 11, 96, 4, 4, padding='VALID', name='conv1')
+#         .lrn(2, 2e-05, 0.75, name='norm1')
+#         .max_pool(3, 3, 2, 2, padding='VALID', name='pool1')
+#         .conv(5, 5, 256, 1, 1, group=2, name='conv2')
+#         .lrn(2, 2e-05, 0.75, name='norm2')
+#         .max_pool(3, 3, 2, 2, padding='VALID', name='pool2')
+#         .conv(3, 3, 384, 1, 1, name='conv3')
+#         .conv(3, 3, 384, 1, 1, group=2, name='conv4')
+#         .conv(3, 3, 256, 1, 1, group=2, name='conv5')
+#         .max_pool(3, 3, 2, 2, padding='VALID', name='pool5')
+#         .fc(4096, name='fc6')
+#         .fc(4096, name='fc7')
+#         .fc(1000, relu=False, name='fc8')
+#         .softmax(name='prob'))
+
+train_x = zeros((1, 241,241,3)).astype(float32)
+train_y = zeros((1, 1000))
+
+net_data = load("bvlc_alexnet.npy").item()
+xdim = train_x.shape[1:]
+ydim = train_y.shape[1]
+
+def conv(input, kernel, biases, k_h, k_w, c_o, s_h, s_w, relu=True, padding="VALID", group=1):
+    c_i = input.get_shape()[-1]
+    assert c_i%group==0
+    assert c_o%group==0
+    convolve = lambda i, k: tf.nn.conv2d(i, k, [1, s_h, s_w, 1], padding=padding)
+    
+    
+    if group==1:
+        conv = convolve(input, kernel)
+    else:
+        input_groups = tf.split(3, group, input)
+        kernel_groups = tf.split(3, group, kernel)
+        output_groups = [convolve(i, k) for i,k in zip(input_groups, kernel_groups)]
+        conv = tf.concat(3, output_groups)
+    return  tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().as_list())
+
+x_dummy = (random.random((1,)+ xdim)/255.).astype(float32)
+x = tf.Variable(x_dummy)
+
+#conv1
+#conv(11, 11, 96, 4, 4, padding='VALID', name='conv1')
+k_h = 11; k_w = 11; c_o = 96; s_h = 4; s_w = 4
+conv1W = tf.Variable(net_data["conv1"][0])
+conv1b = tf.Variable(net_data["conv1"][1])
+conv1_in = conv(x, conv1W, conv1b, k_h, k_w, c_o, s_h, s_w, relu=True, padding="VALID", group=1)
+conv1 = tf.nn.relu(conv1_in)
+
+#lrn1
+#lrn(2, 2e-05, 0.75, name='norm1')
+radius = 2; alpha = 2e-05; beta = 0.75; bias = 1.0
+lrn1 = tf.nn.local_response_normalization(conv1,
+                                                  depth_radius=radius,
+                                                  alpha=alpha,
+                                                  beta=beta,
+                                                  bias=bias)
+
